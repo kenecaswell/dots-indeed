ab725af97ae12e9965687c7c54e55fbe6ee8974e
K.C. Dameron <kene.caswell@gmail.com>
2020-04-27 14:56:43 -0700

    Remove globals and use es6 import

--- src/Controls.js
+++ src/Controls.js
@@ -1,101 +1,102 @@
-(function() {
-  'use strict';
+class Controls {
+  constructor(game, engine) {
+    this.game = game;
+    this.engine = engine;
+    this.isPressed = false;
+    this.startPosition = 0;
+    this.startMouseX = 0;
+    this.pauseButton = document.getElementsByClassName('pause-button')[0];
+    this.sliderContainer = document.getElementsByClassName('game-slider')[0];
+    this.sliderBar = document.getElementsByClassName('slider-bar')[0];
+    this.sliderHandle = document.getElementsByClassName('slider-handle')[0];
+    this.sliderBall = document.getElementsByClassName('slider-ball')[0]
 
-  class Controls {
-    constructor() {
-      this.isPressed = false;
-      this.startPosition = 0;
-      this.startMouseX = 0;
-      this.pauseButton = document.getElementsByClassName('pause-button')[0];
-      this.sliderContainer = document.getElementsByClassName('game-slider')[0];
-      this.sliderBar = document.getElementsByClassName('slider-bar')[0];
-      this.sliderHandle = document.getElementsByClassName('slider-handle')[0];
-      this.sliderBall = document.getElementsByClassName('slider-ball')[0]
+    this.pauseButton.addEventListener('click', this.clickPlayPause.bind(this));
 
-      this.pauseButton.addEventListener('click', this.clickPlayPause);
+    this.sliderHandle.addEventListener('mousedown', (event) => { this.pressSliderHandle(event); })
+    this.sliderHandle.addEventListener('touchstart', (event) => { this.pressSliderHandle(event); })
+    document.addEventListener('mousemove', (event) => { this.moveSliderHandle(event); })
+    document.addEventListener('touchmove', (event) => { this.moveSliderHandle(event); })
+    document.addEventListener('mouseup', (event) => { this.releaseSliderHandle(event); })
+    document.addEventListener('touchend', (event) => { this.releaseSliderHandle(event); })
 
-      this.sliderHandle.addEventListener('mousedown', (event) => { this.pressSliderHandle(event); })
-      this.sliderHandle.addEventListener('touchstart', (event) => { this.pressSliderHandle(event); })
-      document.addEventListener('mousemove', (event) => { this.moveSliderHandle(event); })
-      document.addEventListener('touchmove', (event) => { this.moveSliderHandle(event); })
-      document.addEventListener('mouseup', (event) => { this.releaseSliderHandle(event); })
-      document.addEventListener('touchend', (event) => { this.releaseSliderHandle(event); })
-
-      const mediaWatcher = window.matchMedia('(max-width: 768px)')
-      mediaWatcher.addListener((mediaQuery) => {
-        if (mediaQuery.matches) {
-          this.addResizeListener();
-        } else {
-          this.removeResizeListener();
-        }
-      });
-    }
-
-    clickPlayPause(event) {
-      window.dots.game.togglePause();
-      event.target.innerHTML = (window.dots.game.isPaused) ? 'Play' : 'Pause';
-    }
-
-    pressSliderHandle(event) {
-      const clientX = event.clientX || event.touches[0].clientX;
-      this.startMouseX = clientX - this.sliderContainer.offsetLeft - this.sliderBar.offsetLeft;
-      this.startPosition = this.sliderHandle.offsetLeft;
-      this.isPressed = true;
-
-      this.sliderBall.classList.add('pressed');
-    }
-
-    moveSliderHandle(event) {
-      if (this.isPressed) {
-        const clientX = event.clientX || event.touches[0].clientX;
-        const currentX = clientX - this.sliderContainer.offsetLeft - this.sliderBar.offsetLeft;
-        const deltaX = currentX - this.startMouseX;
-        let newX = this.startPosition + deltaX;
-
-        const minX = this.sliderBar.offsetLeft - this.sliderHandle.clientWidth * .5;
-        const maxX = minX + this.sliderBar.clientWidth;
-        if (newX < minX) newX = minX;
-        if (newX > maxX) newX = maxX;
-
-        this.sliderHandle.style.left = `${newX}px`;
-
-        this.setSpeed(newX, minX, maxX);
+    const mediaWatcher = window.matchMedia('(max-width: 768px)')
+    mediaWatcher.addListener((mediaQuery) => {
+      if (mediaQuery.matches) {
+        this.addResizeListener();
+      } else {
+        this.removeResizeListener();
       }
-    }
+    });
+  }
 
-    releaseSliderHandle(event) {
-      this.isPressed = false;
-      this.sliderBall.classList.remove('pressed');
+  clickPlayPause(event) {
+    this.game.togglePause();
+    if (this.game.isPaused) {
+      this.engine.pause();
+    } else {
+      this.engine.loop();
     }
+    event.target.innerHTML = (this.game.isPaused) ? 'Play' : 'Pause';
+  }
 
-    setSpeed(xPos, minX, maxX) {
-      const normalizePos = xPos - minX;
-      const normalizeMax = maxX - minX;
-      const speedRatio = normalizePos / normalizeMax;
-      window.dots.game.updateSpeed(speedRatio);
-    }
+  pressSliderHandle(event) {
+    const clientX = event.clientX || event.touches[0].clientX;
+    this.startMouseX = clientX - this.sliderContainer.offsetLeft - this.sliderBar.offsetLeft;
+    this.startPosition = this.sliderHandle.offsetLeft;
+    this.isPressed = true;
 
-    addResizeListener() {
-      window.addEventListener('resize', (event) => { this.resetSlideHandlePos(event); });
-      this.resetSlideHandlePos();
-    }
+    this.sliderBall.classList.add('pressed');
+  }
 
-    removeResizeListener() {
-      window.removeEventListener('resize', (event) => { this.resetSlideHandlePos(event); });
-      this.resetSlideHandlePos();
-    }
+  moveSliderHandle(event) {
+    if (this.isPressed) {
+      const clientX = event.clientX || event.touches[0].clientX;
+      const currentX = clientX - this.sliderContainer.offsetLeft - this.sliderBar.offsetLeft;
+      const deltaX = currentX - this.startMouseX;
+      let newX = this.startPosition + deltaX;
 
-    resetSlideHandlePos(event) {
-      const speedRatio = window.dots.game.speedRatio;
       const minX = this.sliderBar.offsetLeft - this.sliderHandle.clientWidth * .5;
-      const maxX = this.sliderBar.clientWidth;
-      const x = minX + (maxX * speedRatio);
+      const maxX = minX + this.sliderBar.clientWidth;
+      if (newX < minX) newX = minX;
+      if (newX > maxX) newX = maxX;
 
-      this.sliderHandle.style.left = `${x}px`;
+      this.sliderHandle.style.left = `${newX}px`;
+
+      this.setSpeed(newX, minX, maxX);
     }
   }
 
-  // Add controls to the dots name space.
-  window.dots.controls = new Controls();
+  releaseSliderHandle(event) {
+    this.isPressed = false;
+    this.sliderBall.classList.remove('pressed');
+  }
 
-})();
+  setSpeed(xPos, minX, maxX) {
+    const normalizePos = xPos - minX;
+    const normalizeMax = maxX - minX;
+    const speedRatio = normalizePos / normalizeMax;
+    this.game.updateSpeed(speedRatio);
+  }
+
+  addResizeListener() {
+    window.addEventListener('resize', (event) => { this.resetSlideHandlePos(event); });
+    this.resetSlideHandlePos();
+  }
+
+  removeResizeListener() {
+    window.removeEventListener('resize', (event) => { this.resetSlideHandlePos(event); });
+    this.resetSlideHandlePos();
+  }
+
+  resetSlideHandlePos(event) {
+    const speedRatio = this.game.speedRatio;
+    const minX = this.sliderBar.offsetLeft - this.sliderHandle.clientWidth * .5;
+    const maxX = this.sliderBar.clientWidth;
+    const x = minX + (maxX * speedRatio);
+
+    this.sliderHandle.style.left = `${x}px`;
+  }
+}
+
+export { Controls }
